a57c538e640838278953b36d374d75190189fa05
Jules-pecquereau <jules.pecquereau4@gmail.com>
2025-04-07 11:05:21 +0200

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,15 +1,19 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SuperMatos</title>
-    <meta name="description" content="">
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <a href="connexion.php">Connexion</a>
-<script src="script.js"></script>
-</body>
-</html>
-
+    <head>
+        <title>SuperMatos</title>
+        <meta charset="UTF-8">
+        <meta name="description" content="">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <header>
+        <div id="logo">
+            <img src="logo_SuperMatos.png" id="logo_app" alt="">
+        </div>
+        <div id="bouton_connexion"></div>
+    </header>
+    <body>
+        
+        <script src="script.js"></script>
+    </body>
+    </html>
