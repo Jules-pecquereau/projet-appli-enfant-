7398345e2641eadd171a8fd9bc340d89a469e45d
Jules-pecquereau <jules.pecquereau4@gmail.com>
2025-04-07 15:27:49 +0200

    connexion et style

--- connexion.php
+++ connexion.php
@@ -7,12 +7,14 @@ $sql="SELECT * FROM `utilisateur`";
 $temp=$pdo->query($sql);
 while($resultat=$temp->fetch()){
     if (isset($_POST["pseudo"]) && isset($_POST["mdp"])){ #vérifie si les valeurs de l'utilisateur sont correct
+        
         if ($_POST["pseudo"] == $resultat["pseudo"] && $_POST["mdp"] == $resultat["mdp"]){
             $_SESSION["connexion"] = 1;
         }
-        else{
-            $erreur = 1 ;
-        }
+    }
+    else{
+        $erreur = 1 ;
+        echo "erreur";
         if ($resultat["admin"] == 1){ #si l'utilisateur est un admin, le faire savoir à la session
             $_SESSION["admin"] = 1;
         }
@@ -21,27 +23,20 @@ while($resultat=$temp->fetch()){
 }
 
 ?>
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Connexion</title>
-    <meta name="description" content="">
-    <link rel="stylesheet" href="style.css">
-</head>
 
 
-</header>
+
 <body>
 <?php
 include("header.php");
 ?>
+<br>
+
 <div id="form_connexion">
         <div id="couleur_form">
             <?php
             if ($_SESSION["connexion"] == 0){ #Vérifie si la personne est connectée ou non
-                echo '<form action="connexion.php" method="post">
+                echo '<form action="connexion.php?connexion='.$_SESSION['connexion'].'" method="post">
                     <div id="pseudo"><input type="text" name="pseudo" placeholder="Pseudo" required></div>
                     <div id="mdp"><input type="password" name="mdp" placeholder="Mot de passe" required></div>
                     <div ><input id="btn" type="submit" name="bouton" value="Valider"></div>
@@ -51,6 +46,7 @@ include("header.php");
             }
             if ($_SESSION["connexion"] == 1){ #si la personne est connectée, renvoie à l'index
                 header('Location: index.php');
+                
             }
 
             ?>
