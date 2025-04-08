69bdae64f7ecc60a81eb97167e6aa56cb7e86f01
Jules-pecquereau <jules.pecquereau4@gmail.com>
2025-04-08 10:18:26 +0200

    index , style

--- Index.php
+++ Index.php
@@ -5,10 +5,12 @@ include('header.php');
 <html>
 <body>
     <form action="index.php">
-        <input type="submit" value="Addition" name="calcul">
-        <input type="submit" value="Soustraction" name="calcul">
-        <input type="submit" value="Multiplication" name="calcul">
-        <input type="submit" value="Division" name="calcul">
+        <div id="menu_index">
+            <div id="addition"><input type="submit" value="Addition" name="calcul"></div>
+            <div id="soustraction"><input type="submit" value="Soustraction" name="calcul"></div>
+            <div id="multiplication"><input type="submit" value="Multiplication" name="calcul"></div>
+            <div id="division"><input type="submit" value="Division" name="calcul"></div>
+        </div>
     </form>
     <?php
     if (isset($_REQUEST["calcul"])){
