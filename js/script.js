4904f5d6c8b3790568cd206089951b94ad72263e
Jules-pecquereau <jules.pecquereau4@gmail.com>
2025-04-09 16:50:45 +0200

    ajout son , script , modif index pour animation js

---
+++ js/script.js
@@ -0,0 +1,25 @@
+let x=0;
+let message =" Bienvenue dans le Monde des Maths !!!!!!!";
+let audio =new Audio("son/parle.mp3");
+message_dalay(x,message);
+function message_dalay(x , message){
+    if (x < message.length ){
+        audio.play();
+        document.getElementById("message").innerHTML=document.getElementById("message").innerHTML+message[x];
+        if(x%2==0){
+            document.getElementById("img_qui_change").src="image/calculette_qui_parle_pas.png";
+        }
+        else{
+            document.getElementById("img_qui_change").src="image/calculette_qui_parle.png";
+        }
+        
+        setTimeout("message_dalay(x++,message)", 50);
+    }
+    else{
+        document.getElementById("img_qui_change").src="image/calculette_qui_parle_pas.png";
+        audio.pause();
+        return;
+
+    }
+}
+
