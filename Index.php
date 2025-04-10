abad2aa6581446510f06006558ff36cc213e73e8
Jules-pecquereau <jules.pecquereau4@gmail.com>
2025-04-10 13:55:55 +0200

    index , style

--- Index.php
+++ Index.php
@@ -10,14 +10,11 @@ $texte = str_split("Boaqz");
         <audio id="audio">
         <source src="son/parle.mp3" type="audio/mpeg">
     </audio>
-        <div>
+        <div id="index_calulette">
             <img src="image/calculette_qui_parle_pas.png" id="img_qui_change" alt="">
-        <div class="bulle"> <p id="message"></p></div>
+        <p id="message" class="bulle"></p>  
         <script src="js/script.js"></script>
     </div>
 </body>
     </html>
 
-<!-- border-radius: 200px 144px 151px 0px;
--webkit-border-radius: 200px 144px 151px 0px;
--moz-border-radius: 200px 144px 151px 0px; -->
