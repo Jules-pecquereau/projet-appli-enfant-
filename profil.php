1f262b9e8d5334c97ec061121335bb13a8d83ac0
Titouan-Lefort <102295079+Titouan-Lefort@users.noreply.github.com>
2025-04-10 15:28:10 +0200

    creation profil

---
+++ profil.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+include("header.php");
+include("bdd.php");
+$sql="SELECT * FROM utilisateur WHERE identifiant=".$_SESSION["id"];
+$temp=$pdo->query($sql);
+while($resultat=$temp->fetch()){
+    echo "<h1>Statistique de ".$resultat["pseudo"]." </h1>";
+}
+
+$sql="SELECT * FROM score WHERE utilisateur=".$_SESSION["id"];
+$temp=$pdo->query($sql);
+while($resultat=$temp->fetch()){
+    $score_addition=$resultat["Addition_Facile"]+$resultat["Addition_Moyen"]+$resultat["Addition_Difficile"];
+    $score_soustraction=$resultat["Soustraction_Facile"]+$resultat["Soustraction_Moyen"]+$resultat["Soustraction_Difficile"];
+    $score_Multiplication=$resultat["Multiplication_Facile"]+$resultat["Multiplication_Moyen"]+$resultat["Multiplication_Difficile"];
+    $score_Division=$resultat["Division_Facile"]+$resultat["Division_Moyen"]+$resultat["Division_Difficile"];
+
+}
+$score_totale=$score_addition+$score_soustraction+$score_Multiplication+$score_Division;
+
+echo "<p>Score total : ".$score_totale."</p>";
+echo "<p>Score total des additions : ".$score_addition."</p><p> Score total des soustractions : ".$score_soustraction."</p>";
+echo "<p>Score total des multiplications : ".$score_Multiplication."<p>Score total des divisions : ".$score_Division."</p>";
+
+?>
+
